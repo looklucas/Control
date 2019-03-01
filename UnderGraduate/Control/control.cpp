902467ee37cdcfeb4d77ae2c02620cc134366a42
looklucas <275678380@qq.com>
2019-03-01 16:50:35 +0800

    version 7
    Show the pressure and fake temperature value in real-time, delete other value;
    output title, original voltage, fake temperature and pressure(AI5)

--- UnderGraduate/Control/control.cpp
+++ UnderGraduate/Control/control.cpp
@@ -99,20 +99,6 @@ void Control::Initialize()
     m_yCordRangeMid_2 = ui->sld_y_center_2->value()/1000.0;
     graph_p->Clear();
 
-
-    str.sprintf("%.1f", ui->sld_y_scale->value()*1.0);
-    ui->lbl_scale_show->setText(str+tr("℃"));
-    str.sprintf("%.1f", ui->sld_y_center->value()*1.0-150.0);
-    ui->lbl_center_show->setText(str+tr("℃"));
-    str.sprintf("%.1f", graph_t->m_yCordRangeMax);
-    ui->lbl_y_max->setText(str+tr("℃"));
-    str.sprintf("%.1f", graph_t->m_yCordRangeMin);
-    ui->lbl_y_min->setText(str+tr("℃"));
-    str.sprintf("%.1f", m_yCordRangeMid);
-    ui->lbl_y_mid->setText(str+tr("℃"));
-    str.sprintf("%.1f", ui->sld_x_scale->value()/100.0);
-    ui->lbl_x_present->setText(str+tr("s/格"));
-
     ui->btn_duty->setEnabled(true);
     ui->btn_open->setEnabled(true);
     ui->btn_close->setEnabled(true);
@@ -122,47 +108,44 @@ void Control::Initialize()
 
     mode = 1;
 
-    ui->lbl_status_pic->setAutoFillBackground(true);
+    ui->lbl_do_pic->setAutoFillBackground(true);
     QPixmap pixMap_open(":/Resources/open.png");
     QPixmap pixMap_close(":/Resources/close.png");
     QPalette backPalette;
     backPalette.setBrush(this->backgroundRole(), QBrush(pixMap_open));
     ui->lbl_di_pic->setPalette(backPalette);
-    ui->lbl_status_pic->setPalette(backPalette);
+    ui->lbl_do_pic->setPalette(backPalette);
 
     ui->listWidget->clear();
     QListWidgetItem *item1 = nullptr;
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 1; i++)
     {
         item1 = new QListWidgetItem(tr("0.00"), ui->listWidget);
-        item1->setBackgroundColor(SimpleGraph::lineColor[i]);
-        item1->setSizeHint(QSize(50, 20));
+        item1->setBackgroundColor(SimpleGraph::lineColor[0]);
+        item1->setSizeHint(QSize(70, 20));
         item1->setTextAlignment(Qt::AlignCenter);
     }
     ui->listWidget_2->clear();
     QListWidgetItem *item2 = nullptr;
-    for (int i = 0; i < 2; i++)
+    for (int i = 0; i < 1; i++)
     {
         item2 = new QListWidgetItem(tr("0.00"), ui->listWidget_2);
-        item2->setBackgroundColor(SimpleGraph::lineColor[i]);
-        item2->setSizeHint(QSize(50, 20));
+        item2->setBackgroundColor(SimpleGraph::lineColor[1]);
+        item2->setSizeHint(QSize(70, 20));
         item2->setTextAlignment(Qt::AlignCenter);
     }
-    /*
-    QListWidgetItem *item2 = nullptr;
-    for (int i = 0; i < configure.channelCount; i++)
-    {
-        item2 = new QListWidgetItem(tr("0.00"), ui->listWidget_click);
-        item2->setBackgroundColor(SimpleGraph::lineColor[i]);
-        item2->setSizeHint(QSize(50, 20));
-        item2->setTextAlignment(Qt::AlignCenter);
-    }*/
     QFile csvFile(OutputPath);
-    qDebug()<<OutputPath;
+    QTextStream textStream(&csvFile);
+    if (csvFile.open(QIODevice::Text | QIODevice::Append))
+    {
+        textStream<<"Real-Time Sampling Data"<<"\t"<<"V1"<<"\t"<<"T(Celsius)"<<"\t"<<"V2"<<"\t"<<"P(MPa)"<<"\t"<<"Do"<<endl;
+        csvFile.close();
+    }
 }
 
 void Control::ConfigureDevice()
 {
+    //Create instant_Ai
     if (instantAiCtrl == nullptr)
     {
       instantAiCtrl = InstantAiCtrl::Create();
@@ -182,6 +165,7 @@ void Control::ConfigureDevice()
         channels->getItem(i).setValueRange(configure.valueRange);
     }
 
+    //Create instant_Di
     if (instantDiCtrl == nullptr)
     {
       instantDiCtrl = InstantDiCtrl::Create();
@@ -191,6 +175,7 @@ void Control::ConfigureDevice()
     errorCodeDi = instantDiCtrl->LoadProfile(configure.profilePath);
     CheckError(errorCodeDi);
 
+    //Create instant_Do
     if (instantDoCtrl == nullptr)
     {
       instantDoCtrl = InstantDoCtrl::Create();
@@ -213,6 +198,7 @@ void Control::CheckError(ErrorCode errorCode)
 
 void Control::TimerTicked()
 {
+    //Read instant_Ai voltage signals
     ErrorCode errorCode = Success;
     errorCode = instantAiCtrl->Read(configure.channelStart, configure.channelCount, scaledData);
     CheckError(errorCode);
@@ -221,29 +207,20 @@ void Control::TimerTicked()
         return;
     }
 
-    Time_total = Time_total+0.01;
-    QString str = tr("");
-    str.sprintf("%.2f", Time_total);
-    ui->lbl_time->setText(str+tr("s"));
-
-    temperature[0] = scaledData[0];
-    temperature[1] = 0;
-    temperature[2] = 0;
-    temperature[3] = 0;
+    //calculate the temperature and pressure frome voltage
+    temperature[0] = (scaledData[0]-2.5)*8.0;
     graph_t->Chart(temperature, 1, 1, 10 / 1000.0);
+    pressure[0] = scaledData[5]/5.0*6.0;
+    graph_p->Chart(pressure, 1, 1, 10 / 1000.0);
 
-    pressure[0] = scaledData[4];
-    double tmp[2];
-    tmp[0] = pressure[0]/5.0*6.0;
-    tmp[1] = 0;
-    graph_p->Chart(tmp, 1, 1, 10 / 1000.0);
-
+    //show the real-time temperature and pressure
+    QString str = tr("");
     QListWidgetItem *item;
     QString dataStr = tr("0.00");
-    for(int i = 0; i < 4; i++)
+    //for(int i = 0; i < 1; i++)
     {
-        item = ui->listWidget->item(i);
-        str.sprintf("%.2f", scaledData[i]);
+        item = ui->listWidget->item(0);
+        str.sprintf("%.2f", temperature[0]);
         dataStr = str;
         if (str.length() > 7)
         {
@@ -251,10 +228,10 @@ void Control::TimerTicked()
         }
         item->setText(dataStr);
     }
-    for(int i = 0; i < 2; i++)
+    //for(int i = 5; i < 6; i++)
     {
-        item = ui->listWidget_2->item(i);
-        str.sprintf("%.2f", pressure[i]);
+        item = ui->listWidget_2->item(0);
+        str.sprintf("%.2f", pressure[0]);
         dataStr = str;
         if (str.length() > 7)
         {
@@ -263,6 +240,7 @@ void Control::TimerTicked()
         item->setText(dataStr);
     }
 
+    //Read and show the status of Di
     QPixmap pixMap_open(":/Resources/open.png");
     QPixmap pixMap_close(":/Resources/close.png");
     QPalette backPalette;
@@ -282,6 +260,12 @@ void Control::TimerTicked()
         ui->lbl_di_pic->setPalette(backPalette);
     }
 
+    //show the current time
+    update_time = QDateTime::currentDateTime();
+    update_time_string = update_time.toString("yyyy_MM_dd_hh_mm_ss_z");
+    ui->lbl_time->setText(update_time.toString("hh")+tr(" : ")+update_time.toString("mm")+tr(" : ")+update_time.toString("ss"));
+
+    //output the time, original voltage, pressure and Do status to txt
     int duty_status;
     if(mode == 1)
         duty_status = 1;
@@ -291,87 +275,22 @@ void Control::TimerTicked()
     {
         duty_status = counter_open>0?1:2;
     }
-    str.sprintf("%.4f", scaledData[4]);
-    //qDebug()<<scaledData[4];
-    //qDebug()<<duty_status;
-    update_time = QDateTime::currentDateTime();
-    update_time_string = update_time.toString("yyyy_MM_dd_hh_mm_ss.zzz");
 
+    QString str_v1 = tr("");
+    str_v1.sprintf("%.4f", scaledData[0]);
+    QString str_v2 = tr("");
+    str_v2.sprintf("%.4f", scaledData[5]);
+    QString str_t;
+    str_t.sprintf("%.4f",temperature[0]);
+    QString str_p;
+    str_p.sprintf("%.4f",pressure[0]);
     QFile csvFile(OutputPath);
     QTextStream textStream(&csvFile);
     if (csvFile.open(QIODevice::Text | QIODevice::Append))
     {
-        textStream<<update_time_string<<"\t"<<str<<"\t"<<duty_status<<endl;
+        textStream<<update_time_string<<"\t"<<str_v1<<"\t"<<str_t<<"\t"<<str_v2<<"\t"<<str_p<<"\t"<<duty_status<<endl;
         csvFile.close();
     }
-}
-
-
-void Control::sld_open_change(int value)
-{
-    QString str = tr("");
-    str.sprintf("%.2f", value/100.0);
-    ui->lbl_open_show->setText(str);
-    old_open = value/100.0;
-    counter_open = qRound((ui->sld_open->value()/100.0)*100);
-    counter_close = qRound((ui->sld_open->value()*ui->sld_ratio->value()/1000.0)*100);
-}
-
-void Control::edit_open_change()
-{
-    double new_open = ui->lbl_open_show->text().toDouble();
-    //qDebug()<<new_open;
-    QString str = tr("");
-    if(new_open<0.1 || new_open>2)
-    {
-        new_open = old_open;
-    }
-    else
-    {
-        old_open = new_open;
-    }
-    str.sprintf("%.2f", new_open);
-    ui->lbl_open_show->setText(str);
-    counter_open = qRound((new_open)*100);
-    counter_close = qRound((new_open*old_ratio)*100);
-    ui->sld_open->setValue(new_open*100.0+0.55);
-    //qDebug()<<new_open;
-    //qDebug()<<ui->sld_open->value();
-}
-
-void Control::sld_ratio_change(int value)
-{
-    QString str = tr("");
-    str.sprintf("%.1f", value/10.0);
-    ui->lbl_ratio_show->setText(str);
-    old_ratio = value/10.0;
-    str.sprintf("%.2f", 1.0/(1.0+ui->sld_ratio->value()/10.0));
-    ui->lbl_ratio_present->setText(str);
-    counter_open = qRound((ui->sld_open->value()/100.0)*100);
-    counter_close = qRound((ui->sld_open->value()*ui->sld_ratio->value()/1000.0)*100);
-}
-
-void Control::edit_ratio_change()
-{
-    double new_ratio = ui->lbl_ratio_show->text().toDouble();
-    //qDebug()<<new_open;
-    QString str = tr("");
-    if(new_ratio<0 || new_ratio>10)
-    {
-        new_ratio = old_open;
-    }
-    else
-    {
-        old_open = new_ratio;
-    }
-    str.sprintf("%.1f", new_ratio);
-    ui->lbl_ratio_show->setText(str);
-    str.sprintf("%.2f", 1.0/(1.0+new_ratio));
-    counter_open = qRound((old_open)*100);
-    counter_close = qRound((old_open*new_ratio)*100);
-    ui->sld_ratio->setValue(new_ratio*10.0+0.55);
-    //qDebug()<<new_ratio;
-    //qDebug()<<ui->sld_ratio->value();
 }
 
 void Control::sld_x_scale_change(int value)
@@ -380,11 +299,11 @@ void Control::sld_x_scale_change(int value)
     graph_p->m_xCordTimeDiv = (value/10.0) * 1000 / 10;
 
     QString str = tr("");
-    str.sprintf("%.1f", ui->sld_x_scale->value()/100.0);
+    str.sprintf("%.1f", ui->sld_x_scale->value()/(1000/100.0));
     ui->lbl_x_present->setText(str+tr("s/格"));
 }
 
-void Control::sld_y_scale_change(int value)
+void Control::sld_y_scale_change(int value)//for graph_t
 {
     graph_t->m_yCordRangeMax = m_yCordRangeMid + value/2.0;
     graph_t->m_yCordRangeMin = m_yCordRangeMid - value/2.0;
@@ -400,7 +319,7 @@ void Control::sld_y_scale_change(int value)
     ui->lbl_y_mid->setText(str+tr("℃"));
 }
 
-void Control::sld_y_scale_change_2(int value)
+void Control::sld_y_scale_change_2(int value)//for graph_p
 {
     graph_p->m_yCordRangeMax = m_yCordRangeMid_2 + value/100.0/2.0;
     graph_p->m_yCordRangeMin = m_yCordRangeMid_2 - value/100.0/2.0;
@@ -416,7 +335,7 @@ void Control::sld_y_scale_change_2(int value)
     ui->lbl_y_mid_2->setText(str+tr("MPa"));
 }
 
-void Control::sld_y_center_change(int value)
+void Control::sld_y_center_change(int value)//for graph_t
 {
     m_yCordRangeMid = value*1.0-150.0;
     graph_t->m_yCordRangeMax = m_yCordRangeMid + ui->sld_y_scale->value()/2.0;
@@ -433,7 +352,7 @@ void Control::sld_y_center_change(int value)
     ui->lbl_y_mid->setText(str+tr("℃"));
 }
 
-void Control::sld_y_center_change_2(int value)
+void Control::sld_y_center_change_2(int value)//for graph_p
 {
     m_yCordRangeMid_2 = value/1000.0;
     graph_p->m_yCordRangeMax = m_yCordRangeMid_2 + ui->sld_y_scale_2->value()/100.0/2.0;
@@ -452,8 +371,8 @@ void Control::sld_y_center_change_2(int value)
 
 void Control::btn_start_click()
 {
-    timer->start(10);
-    timer_control->start(10);
+    timer->start(100);
+    timer_control->start(100);
     ui->btn_temp_start->setEnabled(false);
     ui->btn_temp_pause->setEnabled(true);
     ui->btn_temp_end->setEnabled(true);
@@ -482,8 +401,8 @@ void Control::btn_end_click()
 void Control::btn_duty_click()
 {
     mode = 2;
-    counter_open = qRound((ui->sld_open->value()/100.0)*100);
-    counter_close = qRound((ui->sld_open->value()*ui->sld_ratio->value()/1000.0)*100);
+    counter_open = qRound((ui->sld_open->value()/100.0)*(1000/100.0));
+    counter_close = qRound((ui->sld_open->value()*ui->sld_ratio->value()/1000.0)*(1000/100.0));
 }
 
 void Control::btn_open_click()
@@ -496,6 +415,7 @@ void Control::btn_close_click()
     mode = 3;
 }
 
+//Control mode
 void Control::DutyControl()
 {
     QPixmap pixMap_open(":/Resources/open.png");
@@ -509,7 +429,7 @@ void Control::DutyControl()
         errorCode = instantDoCtrl->Write(0, 1, &data_tmp[0]);
         CheckError(errorCode);
         backPalette.setBrush(this->backgroundRole(), QBrush(pixMap_open));
-        ui->lbl_status_pic->setPalette(backPalette);
+        ui->lbl_do_pic->setPalette(backPalette);
 
     }
     else if (mode == 3)
@@ -520,7 +440,7 @@ void Control::DutyControl()
         errorCode = instantDoCtrl->Write(0, 1, &data_tmp[0]);
         CheckError(errorCode);
         backPalette.setBrush(this->backgroundRole(), QBrush(pixMap_close));
-        ui->lbl_status_pic->setPalette(backPalette);
+        ui->lbl_do_pic->setPalette(backPalette);
     }
     else
     {
@@ -533,7 +453,7 @@ void Control::DutyControl()
             errorCode = instantDoCtrl->Write(0, 1, &data_tmp[0]);
             CheckError(errorCode);
             backPalette.setBrush(this->backgroundRole(), QBrush(pixMap_open));
-            ui->lbl_status_pic->setPalette(backPalette);
+            ui->lbl_do_pic->setPalette(backPalette);
         }
         else if (counter_open == 0 && counter_close>0)
         {
@@ -543,20 +463,87 @@ void Control::DutyControl()
             errorCode = instantDoCtrl->Write(0, 1, &data_tmp[0]);
             CheckError(errorCode);
             backPalette.setBrush(this->backgroundRole(), QBrush(pixMap_close));
-            ui->lbl_status_pic->setPalette(backPalette);
+            ui->lbl_do_pic->setPalette(backPalette);
         }
         else if (counter_open == 0 && counter_close == 0)
         {
-            counter_open = qRound((ui->sld_open->value()/100.0)*100);
-            counter_close = qRound((ui->sld_open->value()*ui->sld_ratio->value()/1000.0)*100);
+            counter_open = qRound((ui->sld_open->value()/100.0)*(1000/100.0));
+            counter_close = qRound((ui->sld_open->value()*ui->sld_ratio->value()/1000.0)*(1000/100.0));
         }
         else
         {
-            counter_open = qRound((ui->sld_open->value()/100.0)*100);
-            counter_close = qRound((ui->sld_open->value()*ui->sld_ratio->value()/1000.0)*100);
+            counter_open = qRound((ui->sld_open->value()/100.0)*(1000/100.0));
+            counter_close = qRound((ui->sld_open->value()*ui->sld_ratio->value()/1000.0)*(1000/100.0));
         }
         qDebug()<<counter_open<<" and "<<counter_close;
 
     }
+}
+
+void Control::sld_open_change(int value)
+{
+    QString str = tr("");
+    str.sprintf("%.2f", value/100.0);
+    ui->lbl_open_show->setText(str);
+    old_open = value/100.0;
+    counter_open = qRound((ui->sld_open->value()/100.0)*(1000/100.0));
+    counter_close = qRound((ui->sld_open->value()*ui->sld_ratio->value()/1000.0)*(1000/100.0));
+}
+
+void Control::edit_open_change()
+{
+    double new_open = ui->lbl_open_show->text().toDouble();
+    //qDebug()<<new_open;
+    QString str = tr("");
+    if(new_open<0.1 || new_open>2)
+    {
+        new_open = old_open;
+    }
+    else
+    {
+        old_open = new_open;
+    }
+    str.sprintf("%.2f", new_open);
+    ui->lbl_open_show->setText(str);
+    counter_open = qRound((new_open)*(1000/100.0));
+    counter_close = qRound((new_open*old_ratio)*(1000/100.0));
+    ui->sld_open->setValue(new_open*100.0+0.55);
+    //qDebug()<<new_open;
+    //qDebug()<<ui->sld_open->value();
+}
+
+void Control::sld_ratio_change(int value)
+{
+    QString str = tr("");
+    str.sprintf("%.1f", value/10.0);
+    ui->lbl_ratio_show->setText(str);
+    old_ratio = value/10.0;
+    str.sprintf("%.2f", 1.0/(1.0+ui->sld_ratio->value()/10.0));
+    ui->lbl_ratio_present->setText(str);
+    counter_open = qRound((ui->sld_open->value()/100.0)*(1000/100.0));
+    counter_close = qRound((ui->sld_open->value()*ui->sld_ratio->value()/1000.0)*(1000/100.0));
+}
+
+void Control::edit_ratio_change()
+{
+    double new_ratio = ui->lbl_ratio_show->text().toDouble();
+    //qDebug()<<new_open;
+    QString str = tr("");
+    if(new_ratio<0 || new_ratio>10)
+    {
+        new_ratio = old_open;
+    }
+    else
+    {
+        old_open = new_ratio;
+    }
+    str.sprintf("%.1f", new_ratio);
+    ui->lbl_ratio_show->setText(str);
+    str.sprintf("%.2f", 1.0/(1.0+new_ratio));
+    counter_open = qRound((old_open)*(1000/100.0));
+    counter_close = qRound((old_open*new_ratio)*(1000/100.0));
+    ui->sld_ratio->setValue(new_ratio*10.0+0.55);
+    //qDebug()<<new_ratio;
+    //qDebug()<<ui->sld_ratio->value();
 }
 
